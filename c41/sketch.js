a56cf79c41e2f75ff4ac62214bb8c671e57e3e30
WhiteHatJrr <73558688+WhiteHatJrr@users.noreply.github.com>
2021-04-09 14:58:44 -0400

    Add files via upload

---
+++ c41/sketch.js
@@ -0,0 +1,46 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var carim1, carim2, carim3, carim4, groundim, trackim;
+
+function preload(){
+  carim1 = loadImage("images/car1.png")
+  carim2 = loadImage("images/car2.png")
+  carim3 = loadImage("images/car3.png")
+  carim4 = loadImage("images/car4.png")
+  groundim = loadImage("images/ground.png")
+  trackim = loadImage("images/track.jpg")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+
+  if(gameState === 2){
+    game.end();
+    //game.update(2)
+  }
+}
